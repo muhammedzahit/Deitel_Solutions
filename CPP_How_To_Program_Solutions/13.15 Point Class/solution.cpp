4c736fb6a099b26446a6beda0e94eed8174b0b5c
Muhammed Zahid <muhammetzahitaydin@gmail.com>
2020-03-24 02:58:19 +0300

    added solution 13.15

---
+++ CPP_How_To_Program_Solutions/13.15 Point Class/solution.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+#include <ios>
+#include <string>
+using namespace std;
+
+class Point{
+    int x;
+    int y;
+public:
+    int getX() const {return x;}
+    int getY() const{return y;}
+    void setY(int a){ y = a ;}
+    void setX(int a){ x = a ;}
+    friend ostream& operator <<(ostream&,Point&);
+    friend istream& operator >>(istream&,Point&);
+};
+
+ostream& operator <<(ostream& out,Point& x){
+    if (x.getX() < 0 || x.getY() < 0){
+        cin.setstate(ios::failbit);
+    }
+    cout << "X location -> " << x.getX() << endl;
+    cout << "Y location -> " << x.getY() << endl;
+}
+
+istream& operator >>(istream& in,Point& x){
+    if (cin.fail()){
+        cout << endl <<  "This point class is closed because of improper data!!!" << endl;
+    }
+    int a;
+    cout << "Enter x location" << endl;
+    cin >> a;
+    x.setX(a);
+    cout << "Enter y location" << endl;
+    cin >> a;
+    x.setY(a);
+}
+
+int main(){
+
+    Point p1;
+    cin >> p1;
+    cout << p1;
+    cin >> p1;
+}
